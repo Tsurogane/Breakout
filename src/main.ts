731f589e5138a82773d7da45f53e328d7c518c29
Tsurogane <pmvdossantos@gmail.com>
2024-05-27 17:24:05 -0300

    feat:Excalibur: Colisões

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Color, Engine, vec } from "excalibur"
+import { Actor, CollisionType, Color, Engine, Font, Text, vec } from "excalibur"
 
 // 1 - Criar uma instancia de Engine, que representa o jogo.
 const game = new Engine({
@@ -12,7 +12,8 @@ const barra = new Actor({
 	y: game.drawHeight - 40,
 	width: 200,
 	height: 20,
-	color: Color.Chartreuse
+	color: Color.Chartreuse,
+	name: "BarraJogador"
 })
 
 barra.body.collisionType = CollisionType.Fixed
@@ -37,7 +38,7 @@ const bolinha = new Actor({
 bolinha.body.collisionType = CollisionType.Passive
 
 // 5 - Criar movimentação bolinha
-const velocidadeBolinha = vec(100, 100)
+const velocidadeBolinha = vec(1000, 100)
 
 // Apos 1 segundo (1000ms), define a velocidade da bolinha em x 
 setTimeout( ()=> {
@@ -80,7 +81,7 @@ const yoffset = 20
 const colunas = 5
 const linhas = 3
 
-const corBloco = [Color.Violet, Color.Orange, Color.Yellow]
+const corBloco = [Color.Red, Color.Orange, Color.Yellow]
 
 const larguraBLoco = (game.drawWidth / colunas) - padding - (padding / colunas)
 // const larguraBloco = 136
@@ -88,6 +89,95 @@ const alturaBloco = 30
 
 const listaBlocos: Actor[] = []
 
+// Renderização dos bloquinhos
+
+// Renderiza 3 linhas
+for(let j = 0; j < linhas; j++) {
+	//Renderiza 5 bloquinhos
+for(let i = 0; i  < colunas; i++) {
+	listaBlocos.push(
+		new Actor({
+			x: xoffset + i * (larguraBLoco + padding) + padding,
+			y: yoffset + j * (alturaBloco + padding) + padding,
+			width: larguraBLoco,
+			height: alturaBloco,
+			color: corBloco[j]
+		})
+	)
+}
+}
+
+listaBlocos.forEach( bloco => {
+	// Deine o tipo de colisor de cada bloco
+	bloco.body.collisionType = CollisionType.Active
+
+	// Adiciona cada bloc no game
+	game.add(bloco)
+})
+
+// Adicionando Pontuação
+let pontos = 0
+
+const textoPontos = new Text( {
+	text: "Hello Wold",
+	font: new Font({})
+})
+
+const objetoTexto = new Actor({
+	x: game.drawHeight - 80 ,
+	y: game.drawHeight - 15
+})
+
+objetoTexto.graphics.use(textoPontos)
+
+game.add(objetoTexto)
+
+let colidindo: boolean = false
+
+
+bolinha.on("collisionstart", (event) => {
+	// Verifica se abolinha colidiu com algun bloco destrutivel
+	console.log(event.other.name);
+
+	// Se o elemento colidido for um bloc da lista de blocos (destrutivel)
+	if ( listaBlocos.includes(event.other)) {
+		//Destruir o bloco colidido
+		event.other.kill()
+	}
+
+	// Rebater a bolinha : Inverter as direceções x e y.
+	let interseccao = event.contact.mtv.normalize()
+
+
+	// Se nao esta colidindo
+	// !colidindo 
+	if (colidindo == false) {
+		colidindo = true
+
+		// Intersecção.x e Intersecção.y
+		// O maior representa o eixo onde houve o contato
+		if (Math.abs( interseccao.x ) > Math.abs(interseccao.y) ) {
+
+			//bolinha.vel.x = -bolinha.vel.x
+			// bolinha.vel.x = *= -1
+			bolinha.vel.x = bolinha.vel.x * -1
+		} else {
+			//bolinha.vel.y = -bolinha.vel.y
+			// bolinha.vel.y = *= -1
+			bolinha.vel.y = bolinha.vel.y * -1
+		}
+	}
+
+})
+
+bolinha.on("collisionend", () => {
+	colidindo = false
+})
+
+bolinha.on("exitviewport", () => {
+	alert("E morreu")
+	window.location.reload()
+})
 
 // Inicia o game
 game.start()
